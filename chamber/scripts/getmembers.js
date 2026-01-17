0b1b3a0597633cef9734769d0d8add5246e1eba9
Brady Burgener <bradylbsee@gmail.com>
2026-01-16 19:16:18 -0700

    directory almost finnished

--- chamber/scripts/getmembers.js
+++ chamber/scripts/getmembers.js
@@ -14,19 +14,24 @@ const displayMembers = (members) => {
         let card = document.createElement('section');
         let name = document.createElement('h3');
         let tag_line = document.createElement('p');
+        let address = document.createElement('p');
         let email = document.createElement('p');
         let phone_number = document.createElement('p');
         let website = document.createElement('p');
         let image = document.createElement('img');
+        let level = document.createElement('p');
 
         name.textContent = member.company_name;
         name.setAttribute('class', 'name');
         
         tag_line.textContent = member.tag_line;
         tag_line.setAttribute('class', 'tagline');
+        
+        address.textContent = member.company_address;
         email.textContent = `Email: ${member.email}`;
         phone_number.textContent = `Phone: ${member.company_phone_number}`;
         website.textContent = `URL: ${member.company_website}`
+        level.textContent = `Membership Level: ${member.membership_level}`;
 
         image.setAttribute('src', member.imageurl);
         image.setAttribute('alt', `Logo of ${member.name}`);
@@ -36,10 +41,12 @@ const displayMembers = (members) => {
 
         card.appendChild(name);
         card.appendChild(tag_line);
+        card.appendChild(image);
+        card.appendChild(address);
         card.appendChild(email);
         card.appendChild(phone_number);
         card.appendChild(website);
-        card.appendChild(image);
+        card.appendChild(level);
 
         cards.appendChild(card);
 
