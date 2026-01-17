18940c4a1eeae2313fd552103d922eaf4c7ea7eb
Brady Burgener <bradylbsee@gmail.com>
2026-01-16 22:42:36 -0700

    update headers

--- chamber/scripts/getmembers.js
+++ chamber/scripts/getmembers.js
@@ -12,7 +12,7 @@ async function getmembers() {
 const displayMembers = (members) => {
     members.forEach((member) => {
         let card = document.createElement('section');
-        let name = document.createElement('h3');
+        let name = document.createElement('h2');
         let tag_line = document.createElement('p');
         let address = document.createElement('p');
         let email = document.createElement('p');
