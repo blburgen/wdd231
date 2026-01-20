8ed116645f359a234c8d708eb3b798012513b5ec
Brady Burgener <bradylbsee@gmail.com>
2026-01-20 15:25:02 -0700

    weather

---
+++ code along/scripts/weather.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=00445076d0a02fd89f4463c8e42387c2';
+//49.750324738051916, 6.639203210995564
+
+async function apiFetch(){
+    try {
+        const response = await fetch(url);
+        if (response.ok){
+            let data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
