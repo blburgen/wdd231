02ccb188fa5c03be3b1db5ce36c3ec357b4e5880
Brady Burgener <bradylbsee@gmail.com>
2026-01-31 12:44:24 -0700

    missing animation

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,6 +1,10 @@
 const year = document.getElementById("currentyear")
+const timestamp = document.getElementById("timestamp")
 
 const today = new Date();
+const loadtime = Date.now();
 
 year.innerHTML = today.getFullYear();
 document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
+
+timestamp.value = loadtime;
---
+++ chamber/scripts/getmembership.js
@@ -0,0 +1,77 @@
+const member_levels = [
+    {
+        "membership_name": "np",
+        "name": "Non Profit Membership Level",
+        "fee": "$0",
+        "benifits": `Listed in the directory<br>
+        Training`
+    },
+    {
+        "membership_name": "bronze",
+        "name": "Bronze Membership Level",
+        "fee": "$100",
+        "benifits": `Listed in the directory<br>
+        Training<br>
+        10% event discounts`
+    },
+    {
+        "membership_name": "silver",
+        "name": "Silver Membership Level",
+        "fee": "$1,000",
+        "benifits": `Listed in the directory<br>
+        Training<br>
+        Listed in spotlight positions on the home page<br>
+        30% event discounts`
+    },
+    {
+        "membership_name": "gold",
+        "name": "Gold Membership Level",
+        "fee": "$10,000",
+        "benifits": `Listed in the directory<br>
+        Training<br>
+        Listed in spotlight positions on the home page<br>
+        50% event discounts <br>
+        yearly special event`
+    }
+
+
+];
+
+const membership = document.querySelector("#membership");
+
+const mydialog = document.querySelector("#mydialog");
+const mytitle = document.querySelector("#mydialog h2");
+const myclose = document.querySelector("#mydialog button");
+const myinfo = document.querySelector("#mydialog p");
+
+myclose.addEventListener("click", () => mydialog.close());
+
+
+function displayItems(data){
+    console.log(data);
+    data.forEach(element => {
+        console.log(element);
+        const div = document.createElement('div');
+        const title = document.createElement('h3');
+        const click_here = document.createElement('button');
+        title.innerHTML = element.name;
+        click_here.innerText = "Learn More";
+        click_here.title = `${element.name} learn more`
+
+
+        click_here.addEventListener("click", () => showStuff(element));
+
+        div.appendChild(title);
+        div.appendChild(click_here);
+        membership.appendChild(div);
+    });
+
+}
+
+displayItems(member_levels);
+
+function showStuff(x){
+    mytitle.innerHTML = x.name;
+    myinfo.innerHTML = `Cost:${x.fee} <br> ${x.benifits}`;
+    mydialog.showModal();
+}
