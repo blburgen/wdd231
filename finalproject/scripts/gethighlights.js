e3060092b43663ef1a68f7209d179bf1aa724e23
Brady Burgener <bradylbsee@gmail.com>
2026-02-14 12:29:05 -0700

    adds link to video

--- finalproject/scripts/gethighlights.js
+++ finalproject/scripts/gethighlights.js
@@ -4,10 +4,15 @@ const url = './data/paintings.json';
 const highlight = document.querySelector('#cards');
 
 async function getmembers() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data.paintings);
-    displayArt(data.paintings);
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.paintings);
+        displayArt(data.paintings);
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 const displayArt = (paintings) => {
